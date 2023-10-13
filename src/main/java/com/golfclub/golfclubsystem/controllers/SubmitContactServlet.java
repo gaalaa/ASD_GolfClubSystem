f892a54b5aea9712490ca822c432abe43224456f
stevenzhang <stevenzhang140@gmail.com>
2023-10-13 21:09:15 +1100

    Contact response

--- src/main/java/com/golfclub/golfclubsystem/controllers/SubmitContactServlet.java
+++ src/main/java/com/golfclub/golfclubsystem/controllers/SubmitContactServlet.java
@@ -15,6 +15,12 @@ public class SubmitContactServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String message = req.getParameter("message");
+        req.setAttribute("name", name);
+        req.setAttribute("email", email);
+        req.setAttribute("message", message);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/submitContact.jsp");
         dispatcher.forward(req, resp);
     }
