4a8306b35cf758605caa8e6ea404bac50853f941
Peter de Vroom <pdevroom@outlook.com>
2023-10-21 15:06:23 +1100

    Add populate products script

---
+++ sql scripts/populateProducts.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product (name, description, price, stock, imageUri)
+VALUES ('Putter', 'Put the balls into the hole.', 49.99, 10, 'img/products/putter.jpg'),
+       ('Driver', 'I got my driver''s license last week.', 89.95, 5, 'img/products/driver.webp'),
+       ('Bag', 'For your clubs, and the club!', 129.50, 2, null)
+;
